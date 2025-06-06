cf48889a6f8744d68880e36e385d11240b14e1e9
carlostoek <carlostoek@gmail.com>
2025-06-06 11:57:51 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,9 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config:
     BOT_TOKEN = os.getenv("BOT_TOKEN")
     ADMIN_ID = int(os.getenv("ADMIN_ID"))
-    # Usando SQLite para pruebas locales. El archivo se creará como gamification.db
-    DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///gamification.db")
     CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
-gamification.d
+
+    # URL de la base de datos
+    DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///gamification.db")
