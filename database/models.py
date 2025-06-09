420a732685ab506e3f484c9f521f0a3a324efc1b
carlostoek <carlostoek@gmail.com>
2025-06-09 11:02:23 -0600

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,4 +1,4 @@
-# from sqlalchemy import Column, Integer, String, BigInteger, DateTime, Boolean, JSON, Text
+from sqlalchemy import Column, Integer, String, BigInteger, DateTime, Boolean, JSON, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
 from sqlalchemy.ext.asyncio import AsyncAttrs
